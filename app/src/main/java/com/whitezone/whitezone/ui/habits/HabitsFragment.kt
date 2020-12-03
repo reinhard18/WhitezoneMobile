f1e0dfe60fdcc4b3261c3796948e97218e5792c0
reinhard18 <reinhard.silaen1@gmail.com>
2020-12-03 18:55:46 +0700

    First commit

---
+++ app/src/main/java/com/whitezone/whitezone/ui/habits/HabitsFragment.kt
@@ -0,0 +1,33 @@
+@file:Suppress("DEPRECATION")
+
+package com.whitezone.whitezone.ui.habits
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProviders
+import com.whitezone.whitezone.R
+
+class HabitsFragment : Fragment() {
+
+    private lateinit var habitsViewModel: HabitsViewModel
+
+    override fun onCreateView(
+            inflater: LayoutInflater,
+            container: ViewGroup?,
+            savedInstanceState: Bundle?
+    ): View? {
+        habitsViewModel =
+                ViewModelProviders.of(this).get(HabitsViewModel::class.java)
+        val root = inflater.inflate(R.layout.fragment_habits, container, false)
+        val textView: TextView = root.findViewById(R.id.text_dashboard)
+        habitsViewModel.text.observe(viewLifecycleOwner, Observer {
+            textView.text = it
+        })
+        return root
+    }
+}
---
+++ app/src/main/java/com/whitezone/whitezone/ui/tips/TipsFragment.kt
@@ -0,0 +1,36 @@
+@file:Suppress("DEPRECATION")
+
+package com.whitezone.whitezone.ui.tips
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProviders
+import com.whitezone.whitezone.R
+
+@Suppress("DEPRECATION")
+class TipsFragment : Fragment() {
+
+    private lateinit var tipsViewModel: TipsViewModel
+
+    override fun onCreateView(
+            inflater: LayoutInflater,
+            container: ViewGroup?,
+            savedInstanceState: Bundle?
+    ): View? {
+        tipsViewModel =
+                ViewModelProviders.of(this).get(TipsViewModel::class.java)
+        val root = inflater.inflate(R.layout.fragment_tips, container, false)
+        val judulTextView: TextView = root.findViewById(R.id.judul)
+        val isiTextView: TextView = root.findViewById(R.id.isi)
+        tipsViewModel.text.observe(viewLifecycleOwner, Observer {
+            judulTextView.text = it
+            isiTextView.text = it
+        })
+        return root
+    }
+}
